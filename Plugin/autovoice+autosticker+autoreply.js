16c0ef5097b835346d8f751434143d9f503e80ad
UNIVERS-gif <univesrmd99@gmail.com>
2025-05-07 10:34:45 +0530

    Update autovoice+autosticker+autoreply.js

--- Plugin/autovoice+autosticker+autoreply.js
+++ Plugin/autovoice+autosticker+autoreply.js
@@ -34,7 +34,7 @@ async (conn, mek, m, { from, body, isOwner }) => {
             
             if (config.AUTO_STICKER === 'true') {
                 //if (isOwner) return;        
-                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'TOHID_MD'},{ quoted: mek })   
+                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'UNI_MDX'},{ quoted: mek })   
             
             }
         }
